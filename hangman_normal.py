bfc094c08b7427548cc3604ec7244345edc44c6d
cjredmond <connor.redmond@gmail.com>
2016-09-15 15:39:07 -0400

    added letter filling to underscores and lose clause

--- hangman_normal.py
+++ hangman_normal.py
@@ -1,30 +1,46 @@
 import random
 with open("sample.txt") as better_open_file:
-    words = better_open_file.read().lower().split()
+    words = better_open_file.read().upper().split()
 
 chosen_word = words[random.randint(0, len(words)-1)]
 length = len(chosen_word)
-print(length)
-print(words)
-print(chosen_word)
+# print(length)
+# print(words)
+# print(chosen_word)
 
 print("The chosen word has {} letters".format(length))
+
+def make_underscores(word):
+    underscores = []
+    for burn in range(0,len(word)):
+        underscores.append("_")
+    return underscores
 
 good_guesses =[]
 bad_guesses =[]
 counter = 0
-game = True
+output = []
+blank_word = make_underscores(chosen_word)
+winning_word = "".join(blank_word).upper()
 
-while counter < 8:
-    counter += 1
-    def make_underscores(word):
-        underscores = []
-        for burn in range(0,len(word)):
-            underscores.append("_")
-        return "".join(underscores)
-    print(make_underscores(chosen_word))
 
-    player_guess = input("What is your guess?   ").lower()
+
+
+while counter < 7 and winning_word != chosen_word:
+
+    print(winning_word)
+    upper_blank = "".join(blank_word)
+    print(upper_blank)
+    print("These letters were already guessed: {}".format(bad_guesses))
+
+
+    player_guess = input("What is your guess?   ").upper()
+
+    for current_location, current_letter in enumerate(chosen_word):
+        if current_letter == player_guess:
+            blank_word[current_location] = player_guess
+
+
 
     if player_guess in chosen_word:
         print("Good guess")
@@ -34,11 +50,9 @@ while counter < 8:
             good_guesses.append(player_guess)
 
     else:
-        print("Bad guess")
+        counter += 1
+        print("You have {} of 7 strikes".format(counter))
         if player_guess in bad_guesses:
             print("You've already guessed that letter")
         else:
             bad_guesses.append(player_guess)
-
-    print(good_guesses)
-    print(bad_guesses)
